8c74feadd5d6c40dfb3c0b9a1e8e7a0d7f077711
BilalAdem <b_attar@estin.dz>
2024-08-28 19:27:23 +0100

    feat: Add Liveblocks integration for real-time collaboration

---
+++ components/ActiveCollaboratorsList.tsx
@@ -0,0 +1,26 @@
+import { useOthers } from "@liveblocks/react/suspense";
+import Image from "next/image";
+import React from "react";
+
+const ActiveCollaboratorsList = () => {
+  const others = useOthers();
+  const collaborators = others.map((other) => other.info);
+  return (
+    <ul className="collaborators-list">
+      {collaborators.map((collaborator) => (
+        <li key={collaborator.id}>
+          <Image
+            src={collaborator.avatar}
+            alt={collaborator.name}
+            width={100}
+            height={100}
+            className="inline-block rounded-full size-8 ring-2 ring-dark-100"
+            style={{ border: `3px solid ${collaborator.color}` }}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default ActiveCollaboratorsList;
